dd683e8ad110dd5e7cbd59f8339fbaa578145e58
yuelei8881-tech <yuelei8881-tech@users.noreply.github.com>
2026-07-16 12:21:39 -0700

    Release v0.78.7 branded print footer

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'zg-auto-erp-shell-v0786';
+const CACHE = 'zg-auto-erp-shell-v0787';
 const SHELL = ['/', '/manifest.webmanifest', '/icons/zg-auto-icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', event => {
